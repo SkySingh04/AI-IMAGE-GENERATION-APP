052fd86c0ccfe77ed74847a0604761a93bc1e191
Akash <akashsingh2210670@gmail.com>
2023-04-20 12:59:53 +0530

    Api ka paisa nahi h

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,8 +14,32 @@ const CreatePost = () => {
     photo : ""
   })
 
-const generateImage =()=>{
+const generateImage = async ()=>{
+  if(form.prompt){
+    try{
+      setGeneratingImg(true)
+      const response = await fetch("http://localhost:8080/api/v1/dalle"
+      ,{
+        method:"POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({prompt : form.prompt})
+      })
 
+      const data = await response.json()
+
+      setForm({...form , photo : `data:image/jpeg;base64,${data.photo}`})
+
+    }
+    catch(error){
+      alert(error)
+    }finally{
+      setGeneratingImg(false)
+    }
+  }else{
+    alert("Please Enter A Prompt")
+  }
 
 }
 
